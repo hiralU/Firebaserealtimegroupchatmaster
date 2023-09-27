dbde48d3058a5ddb2d7e5b714d4408390f3551ef
Hiral Sheth <112160531+hiralU@users.noreply.github.com>
2023-09-27 16:56:01 +0530

    Add files via upload
    Realtime group chat integration with firebase-javascript . This is perfect platform for realtime group chat.
    - Project developed in vs-2022 environment.

---
+++ HomeController.cs
@@ -0,0 +1,32 @@
+﻿using firebase_realtime_chat_master_web_appln.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace firebase_realtime_chat_master_web_appln.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
